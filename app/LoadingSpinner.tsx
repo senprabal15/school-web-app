982f1e418990dd9633f788422690c381ff1fad2a
Prabal Sen <49455558+senprabal15@users.noreply.github.com>
2025-11-02 02:18:30 +0530

    Delete app/LoadingSpinner.tsx

--- app/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex items-center justify-center p-6">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-gray-700"></div>
-    </div>
-  );
-}
